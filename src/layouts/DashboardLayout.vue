5354a793f932e1789b47609818c902f6f2428043
CynthiaNirere <cynthia.nirere@eagles.oc.edu>
2026-02-18 16:01:56 -0600

    added sidebar and layouts

---
+++ src/layouts/DashboardLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <Sidebar v-if="showSidebar" />
+    
+    <v-main>
+      <v-container fluid>
+        <slot></slot>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+import Sidebar from '../components/Sidebar.vue';
+
+const route = useRoute();
+
+// Hide sidebar on login/signup pages
+const showSidebar = computed(() => {
+  const publicRoutes = ['login', 'signup', 'roleSelect'];
+  return !publicRoutes.includes(route.name);
+});
+</script>
